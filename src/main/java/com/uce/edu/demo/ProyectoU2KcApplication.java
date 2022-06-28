3c48892eb29a66315cb909808587e6eb85a18b06
KevinChiguano <kevin_chili1998@hotmail.com>
2022-06-27 20:15:13 -0500

    Ejercicio con JDBC a PostgreSQL

--- src/main/java/com/uce/edu/demo/ProyectoU2KcApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU2KcApplication.java
@@ -2,16 +2,22 @@ package com.uce.edu.demo;
 
 import org.apache.log4j.spi.LoggerFactory;
 import org.jboss.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.sun.tools.javac.Main;
+import com.uce.edu.demo.service.IPersonaJdbcService;
+import com.uce.edu.demo.to.Persona;
 
 @SpringBootApplication
 public class ProyectoU2KcApplication implements CommandLineRunner{
 
-	private static Logger logger = Logger.getLogger(ProyectoU2KcApplication.class);
+	private static final Logger logger = Logger.getLogger(ProyectoU2KcApplication.class);
+	
+	@Autowired
+	private IPersonaJdbcService iPersonaJdbcService;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU2KcApplication.class, args);
@@ -20,8 +26,28 @@ public class ProyectoU2KcApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		logger.info("Hola");
-		logger.error("Hola error");
+		Persona persona = new Persona();
+		persona.setId(4);
+		persona.setNombre("Alexx");
+		persona.setApellido("Guaman");
+		
+		//Insertar
+		//this.iPersonaJdbcService.guardar(persona);
+		
+		Persona per1 = new Persona();
+		per1.setId(1);
+		per1.setNombre("A");
+		per1.setApellido("B");
+		
+		//Actualizar
+		//this.iPersonaJdbcService.actualizar(per1);
+		
+		//Eliminar
+		//this.iPersonaJdbcService.eliminar(3);
+		
+		//Buscar
+		logger.info(this.iPersonaJdbcService.buscar(4));
+		
 	}
 
 }
