784f3d518150f37e891cad08d1cb763e07832f52
KevinChiguano <kevin_chili1998@hotmail.com>
2022-07-19 20:09:55 -0500

    Mapeo JPA OneToOne

--- src/main/java/com/uce/edu/demo/ProyectoU2KcApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU2KcApplication.java
@@ -1,5 +1,6 @@
 package com.uce.edu.demo;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,9 +14,12 @@ import com.uce.edu.demo.estudiante.repository.modelo.Estudiante;
 import com.uce.edu.demo.estudiante.repository.modelo.EstudianteContadorEdad;
 import com.uce.edu.demo.estudiante.repository.modelo.EstudianteSencillo;
 import com.uce.edu.demo.estudiante.service.IEstudianteJpaService;
+import com.uce.edu.demo.repository.modelo.Ciudadano;
+import com.uce.edu.demo.repository.modelo.Empleado;
 import com.uce.edu.demo.repository.modelo.Persona;
 import com.uce.edu.demo.repository.modelo.PersonaContadorGenero;
 import com.uce.edu.demo.repository.modelo.PersonaSencilla;
+import com.uce.edu.demo.service.ICiudadanoService;
 import com.uce.edu.demo.service.IPersonaJpaService;
 
 @SpringBootApplication
@@ -24,10 +28,7 @@ public class ProyectoU2KcApplication implements CommandLineRunner{
 	private static final Logger LOGGER = Logger.getLogger(ProyectoU2KcApplication.class);
 	
 	@Autowired
-	private IPersonaJpaService iPersonaJpaService;
-	
-	@Autowired
-	private IEstudianteJpaService estudianteJpaService;
+	private ICiudadanoService ciudadanoService;
 		
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU2KcApplication.class, args);
@@ -36,13 +37,30 @@ public class ProyectoU2KcApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		
-		LOGGER.info("Estudiante: "+this.estudianteJpaService.buscarPorCedulaSencillo("123789456"));
+		Ciudadano ciudadano = new Ciudadano();
+		ciudadano.setApellido("Cayambe");
+		ciudadano.setNombre("David");
 		
-		List<EstudianteContadorEdad> listaEstudianteEdad = this.estudianteJpaService.consultarCantidadPorEdad();
+		Empleado empl1 = new Empleado();
+		empl1.setCodigoIess("456456");
+		empl1.setSalario(new BigDecimal(200));
+		empl1.setCiudadano(ciudadano);
 		
-		for(EstudianteContadorEdad item : listaEstudianteEdad) {
-			LOGGER.info(item);
-		}
+		ciudadano.setEmpleado(empl1);
+		
+		this.ciudadanoService.insertar(ciudadano);
+		
+		Ciudadano ciudadano2 = new Ciudadano();
+		ciudadano2.setApellido("Cayambe2");
+		ciudadano2.setNombre("David2");
+		
+		Empleado empl2 = new Empleado();
+		empl2.setCodigoIess("456456");
+		empl2.setSalario(new BigDecimal(202));
+		empl2.setCiudadano(ciudadano);
+		
+		//ciudadano.setEmpleado(empl1);
+		
 	}
 
 }
